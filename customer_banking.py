ee0d7c2dd03a50e15904ebab5bd4c498ebe3954b
Creaitive-Thinking <152429947+CREAITIVE-THINKING@users.noreply.github.com>
2023-12-15 11:29:44 -0500

    final version

--- customer_banking.py
+++ customer_banking.py
@@ -31,18 +31,18 @@ def main():
     updated_savings_balance, savings_interest_earned = create_savings_account(savings_balance, savings_interest, savings_months)
     
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
-    print(f'Savings Account: Updated Balance: ${updated_savings_balance:.2f}, Interest Earned: ${savings_interest_earned:.2f}')
+    print(f'Savings Account: Updated Balance: ${updated_savings_balance:,.2f}, Interest Earned: ${savings_interest_earned:,.2f}')
     
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
-    cd_balance = float(input('Enter CD account balance: '))
-    cd_interest = float(input('Enter CD account interest rate (APR %): '))
-    cd_months = int(input('Enter the number of months for the CD account: '))
+    cd_balance = parse_float('Enter CD account balance: ')
+    cd_interest = parse_float('Enter CD account interest rate (APR %): ')
+    cd_months = parse_int('Enter the number of months for the CD account: ')
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, cd_interest_earned = create_cd_account(cd_balance, cd_interest, cd_months)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
-    print(f'CD Account: Updated Balance: ${updated_cd_balance:.2f}, Interest Earned: ${cd_interest_earned:.2f}')
+    print(f'CD Account: Updated Balance: ${updated_cd_balance:,.2f}, Interest Earned: ${cd_interest_earned:,.2f}')
 
 if __name__ == "__main__":
     # Call the main function.
